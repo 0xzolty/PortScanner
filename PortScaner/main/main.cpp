5a6d035ef6259bd74e3f87697a50f50e095bce57
0xzolty <igorignasiak.facebook@gmail.com>
2026-08-08 20:06:50 +0200

    .

--- PortScaner/main/main.cpp
+++ PortScaner/main/main.cpp
@@ -3,76 +3,82 @@
 #pragma comment(lib, "ws2_32.lib")
 #include <iostream>
 #include <string>
+#include <array>
+
 
 int main() {
 	WSADATA wsaData;
-	
-	// initialize Winsock 2.2 result holds the error code
+// initialize Winsock 2.2 result holds the error code
 
-	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	
-	// check if winsock work 
-	if (result != 0) {
-		std::cerr << "fail ,fail code : "<< result << std::endl;
-		getchar();
-		return 1;
+int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	}
+// check if winsock work 
+
+if (result != 0) {
+	std::cerr << "fail ,fail code : " << result << std::endl;
+	getchar();
+	return 1;
+}
+
+// geting port to scan from user 
+
+int portfrom, portto;
+
+std::cout << "give 1st port to scan : " << std::endl;
+std::cin >> portfrom;
+
+std::cout << "give last port to scan : " << std::endl;
+std::cin >> portto;
+
+// chose destenity 
+
+std::string ip_port;
+
+// geting ip addres to scan from uuser 
+
+std::cout << "ip addres in format : '127.0.0.1'" << std::endl;
+std::cin >> ip_port;
+
+if (portfrom < 1 || portto > 65535 || portfrom > portto) {
+	std::cout << "wrong port must be in range of 1 - 65535" << std::endl;
+	WSACleanup();
+	return 1;
+}
+
+for (int port = portfrom; port <= portto; ++port) {
+	std::cout << "scaning ports " << port << std::endl;
 
 	// initialize socket with ipv4, reliable transmission, tcp and error check
 
 	SOCKET soc = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
 	if (soc == INVALID_SOCKET) {
 		std::cerr << "fail to connect ,fail code : " << WSAGetLastError() << std::endl;
-		getchar();
-		WSACleanup();
-		return 1;
 
-	}
-
-	// geting port to scan from user 
-	
-	int port;
-	std::cout << "give port to scan : " << std::endl;
-	std::cin >> port;
-
-	if (port >= 65535 || port <= 1) {
-		std::cout << "wrong port must be in range of 1 - 65535" << std::endl;
-		getchar();
-		closesocket(soc);
 		WSACleanup();
 		return 1;
 	}
 
 	// create target virable with sockaddr_in 
-	
+
 	sockaddr_in target{};
 	target.sin_family = AF_INET;
 	target.sin_port = htons(port);
-
-	// chose destenity 
-
-	std::string ip_port;
-	
-	// geting ip addres to scan from uuser 
-
-	std::cout << "ip addres in format : '127.0.0.1'" << std::endl;
-	std::cin >> ip_port;
 
 	int addrResult = inet_pton(AF_INET, ip_port.c_str(), &target.sin_addr);
 
 	// error / user issue check 
 
 	if (addrResult == 0) {
-		std::cerr << "wrong ip format, please put correct one eg. 127.0.0.1" << std::endl;
-		getchar();
+		std::cerr<< "wrong ip format, please put correct one eg. 127.0.0.1" << std::endl;
+
 		closesocket(soc);
 		WSACleanup();
 		return 1;
 	}
 	else if (addrResult == -1) {
 		std::cerr << "error: " << WSAGetLastError() << std::endl;
-		getchar();
+
 		closesocket(soc);
 		WSACleanup();
 		return 1;
@@ -81,21 +87,22 @@ int main() {
 	// establish connection check if port is open 
 
 	int con = connect(soc, (sockaddr*)&target, sizeof(target));
+
 	if (con == SOCKET_ERROR) {
 		std::cerr << "error " << WSAGetLastError() << std::endl;
-		getchar();
 	}
-	else{
+	else {
 		std::cout << "port open" << std::endl;
 	}
-	//close socket 
+
+	// close socket 
 
 	closesocket(soc);
-	
-	// clean socket api 
-
-	WSACleanup();
-
-	getchar();
-	return 0;
 }
+
+// clean socket api 
+
+WSACleanup();
+
+getchar();
+return 0;
